57a0a66077a14badbfc68928fee34728ec836e6c
borisovvchs <oriz199362@gmail.com>
2020-07-16 18:26:45 +0300

    hide api key2

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
-require('dotenv').config();
 
 const app = express();
 
