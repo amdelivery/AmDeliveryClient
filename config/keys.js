0caed52b0e77690f1aaa05dc265ca43027b7691d
amdelivery <69623997+amdelivery@users.noreply.github.com>
2020-08-13 11:37:43 +0300

    Delete keys.js

--- config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://DimaNedelin:555@cluster0-vtsxn.mongodb.net/DataForApp?retryWrites=true&w=majority',
-    userName: 'amdelivery-11008-api',
-    password: 'Sdelaicf1993!'
-
-}
